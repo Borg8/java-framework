d61864b184f07715e1a134ae0156667cf8be8068
Borg <mdborg@gmail.com>
2019-09-21 22:46:38 +0300

    NN append

--- src/borg/framework/auxiliaries/Auxiliary.java
+++ src/borg/framework/auxiliaries/Auxiliary.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
 
 public final class Auxiliary
 {
@@ -28,9 +29,7 @@ public final class Auxiliary
 	// Fields
 	//////////////////////////////////////////////////////////////////////////////////////////////////
 
-	private static int sRandomLWord;
-
-	private static int sRandomHWord;
+	private static final Random sRandom = new Random();
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////
 	// Methods
@@ -48,53 +47,39 @@ public final class Auxiliary
 	}
 
 	/**
-	 * reset random pseudo algorithm to some initial value. Initialization will failed if low value is
-	 * 0 or 0x464fffff or high value is 0 or 0x9068ffff.
+	 * reset random pseudo algorithm to some initial value.
 	 *
-	 * @param l_ low initialization word. May not be 0 or 0x464fffff.
-	 * @param h_ high initialization word. May not be 0 or 0x9068ffff.
-	 */
-	public static void resetRandom(int l_, int h_)
-	{
-		// if initiate values is valid
-		if ((l_ != 0) && (l_ != 0x464fffff) && (h_ != 0) && (h_ != 0x9068ffff))
-		{
-			sRandomLWord = l_;
-			sRandomHWord = h_;
-		}
-	}
-
-	/**
-	 * reset random pseudo algorithm to some initial value. Initialization will be failed if reset
-	 * value is 0 or 0x9068ffff464fffffL.
-	 *
-	 * @param reset_ initialization value. May not be 0 or 0x9068ffff464fffffL.
+	 * @param reset_ initialization value.
 	 */
 	public static void resetRandom(long reset_)
 	{
 		// if reset value is valid
 		if ((reset_ != 0) && (reset_ != 0x9068ffff464fffffL))
 		{
-			sRandomLWord = (int)(reset_);
-			sRandomHWord = (int)(reset_ >> 32);
+			sRandom.setSeed(reset_);
 		}
 	}
 
 	/**
 	 * @return pseudo random unsigned numbers in 31 bit
 	 */
+	@Contract(pure = true)
 	public static int random()
 	{
-		sRandomHWord = 36969 * (sRandomHWord & 0xffff) + (sRandomHWord >> 16);
-		sRandomLWord = 18000 * (sRandomLWord & 0xffff) + (sRandomLWord >> 16);
+		int random = sRandom.nextInt();
+		if (random < 0)
+		{
+			return -random;
+		}
 
-		return ((sRandomHWord << 16) + sRandomLWord) & 0x7fffffff;
+		return random;
 	}
 
 	/**
 	 * read bytes from input stream.
-	 * 
+	 *
 	 * @param stream_ stream to read from.
+	 *
 	 * @return read bytes or {@code null} if the stream is not readable.
 	 */
 	@Nullable
@@ -107,7 +92,7 @@ public final class Auxiliary
 
 		try
 		{
-			for (;;)
+			for (; ; )
 			{
 				// read chunk
 				int len;
@@ -153,7 +138,7 @@ public final class Auxiliary
 
 	/**
 	 * sleep on thread.
-	 * 
+	 *
 	 * @param time_ how much time to sleep.
 	 */
 	public static void sleep(long time_)
